b60ef4a787df627e4a6ef4c43c2ed6167bc1344f
nathan1658 <pooikei165@gmail.com>
2019-09-07 10:37:50 +0800

    Initial commit

---
+++ Main/Main/Program.cs
@@ -0,0 +1,294 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Main
+{
+    class Mapping
+    {
+        private string _sid;
+
+        public string Sid
+        {
+            get { return _sid; }
+            set
+            {
+                _sid = value;
+                try
+                {
+                    Username = new System.Security.Principal.SecurityIdentifier(_sid).Translate(typeof(System.Security.Principal.NTAccount)).ToString();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Invalid Username...{ex}");
+                    Username = _sid;
+                }
+            }
+        }
+
+        public string Username;
+        public override string ToString()
+        {
+            return $"{Username} : {Path}";
+        }
+        public string Path;
+
+
+        public string Version { get; set; }
+    }
+    class Program
+    {
+        const string OST_PATH = @"Software\Microsoft\Windows NT\CurrentVersion\Windows Messaging Subsystem\Profiles\Outlook";
+
+        static readonly Dictionary<string, string> PSTPaths = new Dictionary<string, string>
+        {
+            ["2007"] = @"software\Microsoft\Office\12.0\Outlook",
+            ["2010"] = @"software\Microsoft\Office\14.0\Outlook",
+            ["2013"] = @"software\Microsoft\Office\15.0\Outlook",
+            ["2016"] = @"software\Microsoft\Office\16.0\Outlook",
+        };
+
+
+        const string OST_KEY = "001f6610";
+        static List<Mapping> result = new List<Mapping>();
+        static Dictionary<string, string> ProfilePathSidDict = new Dictionary<string, string>();
+        static void Main(string[] args)
+        {
+
+            bool isXP = System.Environment.OSVersion.Version.Major == 5;
+            string user_path = isXP ? "C:\\Documents and Settings" : "C:\\Users";
+
+
+
+            //construct profile key..
+            var profileSubKey = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\ProfileList");
+            foreach (var key in GetKeys(profileSubKey))
+            {
+                var profileKey = profileSubKey.OpenSubKey(key);
+                var profilePath = (string)profileKey.GetValue("ProfileImagePath");
+                string resolvedVal = key;
+                try
+                {
+                    resolvedVal = new System.Security.Principal.SecurityIdentifier(key).Translate(typeof(System.Security.Principal.NTAccount)).ToString();
+                }
+                catch (Exception ex)
+                {
+
+                }
+                ProfilePathSidDict[profilePath] = resolvedVal;
+            }
+
+
+
+            string filePath = "./";
+            if (args != null && args.Length > 0)
+            {
+                filePath = args[0];
+            }
+
+            OnlineUsersGetOstAndPst();
+
+            if (Directory.Exists(user_path))
+            {
+                string[] sub_dirs = Directory.GetDirectories(user_path);
+               // int i = 0;
+                foreach (string dir in sub_dirs)
+                {
+                  //  RegistryInterop.Unload("test" + i);
+
+                    var domainName = dir;
+                    if (ProfilePathSidDict.ContainsKey(dir))
+                    {
+                        domainName = ProfilePathSidDict[dir];
+                    }
+                    Console.WriteLine("Directory is:" + dir);
+
+                    string wimHivePath = Path.Combine(dir, "ntuser.dat");
+                    string copy = Path.Combine(dir, "ntuserCOPY.dat");
+
+                    try
+                    {
+
+                        File.Copy(wimHivePath, copy, true);
+
+                        string loadedHiveKey = RegistryInterop.Load(copy);
+                        if (string.IsNullOrEmpty(loadedHiveKey))
+                        {
+                            RegistryInterop.Unload();
+                            continue;
+                        }
+
+                        RegistryKey rk = Microsoft.Win32.Registry.Users.OpenSubKey(loadedHiveKey);
+
+                        if (rk != null)
+                        {
+                            ReadFromRootKey(rk, domainName);
+                            rk.Close();
+
+                        }
+
+                        File.Delete(copy);
+                    }
+                    catch (Exception ex)
+                    {
+                        RegistryInterop.Unload();
+                        //  Console.WriteLine(ex);
+                        continue;
+                    }
+
+                    RegistryInterop.Unload();
+
+                }
+            }
+
+            var machineName = System.Environment.MachineName;
+
+
+
+            Console.WriteLine("---------------------------------------------");
+
+            var csv = new StringBuilder();
+            //contains all ost and pst..
+            foreach (var ss in result)
+            {
+                Console.WriteLine(ss);
+                var s1 = $"{machineName},{ss.Username},{ss.Path},{ss.Version}";
+                csv.AppendLine(s1);
+            }
+            System.IO.File.WriteAllText($"{filePath}\\{machineName}.csv", csv.ToString());
+
+            Console.WriteLine("Done.");
+        }
+
+        public static void OnlineUsersGetOstAndPst()
+        {
+            RegistryKey rk = RegistryKey.OpenRemoteBaseKey(RegistryHive.Users, "");
+            foreach (var s in rk.GetSubKeyNames())
+            {
+                var userRootKey = rk.OpenSubKey(s);
+                if (userRootKey != null)
+                {
+                    ReadFromRootKey(userRootKey, s);
+                    userRootKey.Close();
+                }
+            }
+            rk.Close();
+
+        }
+
+        public static void ReadFromRootKey(RegistryKey rootKey, string sid)
+        {
+
+            if (rootKey != null)
+            {
+                var ostRootKey = rootKey.OpenSubKey(OST_PATH);
+                if (ostRootKey != null)
+                {
+                    foreach (var path in GetKeys(ostRootKey))
+                    {
+                        var tmpList = ostRootKey.OpenSubKey(path);
+                        var ostPathArr = tmpList.GetValue(OST_KEY);
+                        if (ostPathArr != null)//OST PATH found, convert it 
+                        {
+                            //Unicode..
+                            var realPath = System.Text.Encoding.Unicode.GetString((byte[])ostPathArr);
+                            //Console.WriteLine(realPath);
+                            result.Add(new Mapping { Sid = sid, Path = realPath });
+                            continue;
+                        }
+                        tmpList.Close();
+                    }
+                    ostRootKey.Close();
+                }
+
+                //Get PST
+                foreach (var kvp in PSTPaths)
+                {
+                    var path = kvp.Value;
+                    var tmpRootKey = rootKey.OpenSubKey(path);
+                    if (tmpRootKey != null)
+                    {
+                        var _result = OutputRegKey(tmpRootKey).Where(x => x.Contains("pst")).ToList();
+                        if (_result != null)
+                        {
+                            foreach (var a in _result.Distinct())
+                            {
+                                result.Add(new Mapping { Sid = sid, Path = a, Version = kvp.Key });
+                            }
+                        }
+                        tmpRootKey.Close();
+                    }
+                };
+            }
+
+
+
+        }
+
+        private static List<string> processValueNames(RegistryKey Key)
+        { //function to process the valueNames for a given key
+            string[] valuenames = Key.GetValueNames();
+            if (valuenames == null || valuenames.Length <= 0) //has no values
+                return new List<string>();
+            return valuenames.ToList();
+        }
+
+        public static List<string> OutputRegKey(RegistryKey Key)
+        {
+            try
+            {
+                List<string> resultList = new List<string>();
+
+                string[] subkeynames = Key.GetSubKeyNames(); //means deeper folder
+                if (subkeynames == null || subkeynames.Length <= 0)
+                { //has no more subkey, process
+                    resultList.AddRange(processValueNames(Key));
+                    return resultList;
+                }
+                foreach (string keyname in subkeynames)
+                { //has subkeys, go deeper
+                    using (RegistryKey key2 = Key.OpenSubKey(keyname))
+                        resultList.AddRange(OutputRegKey(key2));
+                }
+                if (processValueNames(Key) != null)
+                {
+                    resultList.AddRange(processValueNames(Key));
+                }
+                Key.Close();
+                return resultList;
+            }
+            catch (Exception e)
+            {
+                // Console.WriteLine("Exception...", e);
+            }
+            Key.Close();
+            return new List<string>();
+        }
+
+        static List<string> GetKeys(RegistryKey rkey, int limit = 100)
+        {
+
+            // Retrieve all the subkeys for the specified key.
+            String[] names = rkey.GetSubKeyNames();
+
+            int icount = 0;
+
+            var resultList = new List<String>();
+            // Print the contents of the array to the console.
+            foreach (String s in names)
+            {
+                resultList.Add(s);
+                // The following code puts a limit on the number
+                // of keys displayed.  Comment it out to print the
+                // complete list.
+                icount++;
+                //if (icount >= limit)
+                //    break;
+            }
+            return resultList;
+        }
+    }
+}
---
+++ Main/Main/RegistryInterop.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Main
+{
+    // Registry class for loading NTUser.dat file and 
+    // temporary hive key and then unloading temporary hive key.
+
+    public class RegistryInterop
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID
+        {
+            public int LowPart;
+            public int HighPart;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TOKEN_PRIVILEGES
+        {
+            public LUID Luid;
+            public int Attributes;
+            public int PrivilegeCount;
+        }
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto)]
+        public static extern int OpenProcessToken(int ProcessHandle, int DesiredAccess, ref int tokenhandle);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetCurrentProcess();
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto)]
+        public static extern int LookupPrivilegeValue(string lpsystemname, string lpname, [MarshalAs(UnmanagedType.Struct)] ref LUID lpLuid);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto)]
+        public static extern int AdjustTokenPrivileges(int tokenhandle, int disableprivs, [MarshalAs(UnmanagedType.Struct)]ref TOKEN_PRIVILEGES Newstate, int bufferlength, int PreivousState, int Returnlength);
+
+        [DllImport("advapi32.dll")]
+        public static extern int RegLoadKey(uint hKey, string lpSubKey, string lpFile);
+
+        [DllImport("advapi32.dll")]
+        public static extern int RegUnLoadKey(uint hKey, string lpSubKey);
+
+        public const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+        public const int TOKEN_QUERY = 0x00000008;
+        public const int SE_PRIVILEGE_ENABLED = 0x00000002;
+        public const string SE_RESTORE_NAME = "SeRestorePrivilege";
+        public const string SE_BACKUP_NAME = "SeBackupPrivilege";
+        public const uint HKEY_USERS = 0x80000003;
+
+        //temporary hive key
+        public const string HIVE_SUBKEY = "Test";
+
+
+        static private Boolean gotPrivileges = false;
+
+        static private void GetPrivileges()
+        {
+
+
+            int token = 0;
+
+            int retval = 0;
+
+            TOKEN_PRIVILEGES TP = new TOKEN_PRIVILEGES();
+
+            TOKEN_PRIVILEGES TP2 = new TOKEN_PRIVILEGES();
+
+            LUID RestoreLuid = new LUID();
+
+            LUID BackupLuid = new LUID();
+
+            retval = OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref token);
+
+            retval = LookupPrivilegeValue(null, SE_RESTORE_NAME, ref RestoreLuid);
+
+            retval = LookupPrivilegeValue(null, SE_BACKUP_NAME, ref BackupLuid);
+
+            TP.PrivilegeCount = 1;
+
+            TP.Attributes = SE_PRIVILEGE_ENABLED;
+
+            TP.Luid = RestoreLuid;
+
+            TP2.PrivilegeCount = 1;
+
+            TP2.Attributes = SE_PRIVILEGE_ENABLED;
+
+            TP2.Luid = BackupLuid;
+
+            retval = AdjustTokenPrivileges(token, 0, ref TP, 1024, 0, 0);
+
+            retval = AdjustTokenPrivileges(token, 0, ref TP2, 1024, 0, 0);
+
+            gotPrivileges = true;
+        }
+
+        static public string Load(string file)
+        {
+            if (!gotPrivileges)
+                GetPrivileges();
+            var result = RegLoadKey(HKEY_USERS, HIVE_SUBKEY, file);
+            Console.WriteLine("Load return " + result);
+            if (result != 0)//Fail
+                return null;
+            return HIVE_SUBKEY;
+            //RegLoadKey(HKEY_USERS, HIVE_SUBKEY, file);
+            //return HIVE_SUBKEY;
+        }
+
+        static public void Unload()
+        {
+            if (!gotPrivileges)
+                GetPrivileges();
+            int output = RegUnLoadKey(HKEY_USERS, HIVE_SUBKEY);
+            Console.WriteLine("Unload return " + output);
+        }
+    }
+}
